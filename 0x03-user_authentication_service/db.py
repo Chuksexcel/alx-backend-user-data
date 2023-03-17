6105a93a20efa4cefa1d8c416347e2f063cb4551
Chuksexcel <Mekabanz@gmail.com>
2023-03-17 22:49:48 +0000

    update_user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,54 +1,56 @@
 #!/usr/bin/env python3
-"""DB module
-"""
+""" Create user, Find user, Update user """
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-
+from sqlalchemy.orm.exc import NoResultFound
+from typing import TypeVar
 from user import Base, User
-import typing
 
 
-class DB(User):
-    """DB class
-    """
-    id = User.id
-
-    def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+class DB:
+    """ class """
+    def __init__(self):
+        """ constructor """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
-        """
+    def _session(self):
+        """ create a session """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
-            return self.__session
+        return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """add_user
-
-        Args:
-            from User.email as email
-            from User.hashed_password as hashed_password
-
-        Returns:
-            user as an object in db from email with hashed_password
-        """
-        self.email = email
-        self.hashed_password = hashed_password
+        """ save the user to the database and returns a User object """
         user = User(email=email, hashed_password=hashed_password)
-        my_session = self._session
-        try:
-            my_session.add(user)
-            my_session.commit()
-        except Exception:
-            user = None
+        self._session.add(user)
+        self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ takes in arbitrary keyword arguments and returns the first row
+            found in the users table as filtered by the method’s input
+            arguments """
+        if kwargs is None:
+            raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ locate the user to update, then will update the user’s attributes
+            as passed in the method’s arguments then commit changes to the
+            database """
+        usr = self.find_user_by(id=user_id)
+        for k, v in kwargs.items():
+            if not hasattr(usr, k):
+                raise ValueError
+            setattr(usr, k, v)
+        self._session.commit()
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,27 +1,14 @@
 #!/usr/bin/env python3
-'''creating a model of users
-'''
-from sqlalchemy import Integer, Column, String
+""" User model """
 from sqlalchemy.ext.declarative import declarative_base
-
-
+from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 
 class User(Base):
-    '''
-        - Model for users
-        Return:
-                - users
-                - users.id: INTEGER
-                - users.email: VARCHAR(250)
-                - users.hashed_password: VARCHAR(250)
-                - users.session_id: VARCHAR(250)
-                - users.reset_token: VARCHAR(250)
-    '''
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    """ SQLAlchemy model for a database table named users """
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
